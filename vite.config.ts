193384930a336a0042f64d39b4ee4b3765d118b8
Miguel Angel <miguelangel@MacBook-Pro.local>
2022-06-17 20:29:28 +0200

    chore: Integrate travis to deploy pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/booking-app/',
   plugins: [react(), ViteAliases({ useTypescript: true, prefix: 'src/' })],
 });
