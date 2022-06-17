1de569e5c669f733cde01bbd8bfa94c71908abde
Miguel Angel <miguelangel@MacBook-Pro.local>
2022-06-17 20:16:11 +0200

    feat: Include alt description to picture

--- src/components/Carrousel/index.tsx
+++ src/components/Carrousel/index.tsx
@@ -36,7 +36,10 @@ const Carrousel: FC<ICarrouselProps> = ({ items = [] }) => {
             key={`carrousel-key-${index}`}
             data-testid={`carrousel-item-${index}`}
           >
-            <img src={`${url}?w=1000&ar=2:1`} />
+            <img
+              src={`${url}?w=1000&ar=2:1`}
+              alt={`imagen displayed Nº ${index}`}
+            />
           </CarrouselItem>
         ))}
       </CarrouselInner>
